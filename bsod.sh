588a2f45bf2ad6a4bb4ee46490f07638bb0bc1b8
Cat Weird <52168620+catware1@users.noreply.github.com>
2021-05-11 11:20:13 +0300

    Update bsod.sh

--- bsod.sh
+++ bsod.sh
@@ -1,18 +1,18 @@
-echo -e '\e[44m                                                                                  \e[49m
-> \e[44m                                                                                  \e[49m
-> \e[44m                                                                                  \e[49m
-> \e[44m                              \e[47m\e[34m Debian \e[44m                                            \e[49m
-> \e[44m                                                                                  \e[49m
-> \e[44m           \e[39mAn exception 0E has occured at 0028:C7HE38B4 in DEBIAN GNU +           \e[49m
-> \e[44m           LINUX. This was called from 0xDED:DF7D4 in VFX:43J5G7H.                \e[49m
-> \e[44m           It may be possible to continue normally.                               \e[49m
-> \e[44m                                                                                  \e[49m
-> \e[44m           * Press any key to attempt to continue.                                \e[49m
-> \e[44m           * Press CTRL+ALT+DEL to restart your computer. You will                \e[49m
-> \e[44m           lose any unsaved information in all applications.                      \e[49m
-> \e[44m                                                                                  \e[49m
-> \e[44m                      Press any key to continue _                                 \e[49m
-> \e[44m                                                                                  \e[49m
-> \e[44m                                                                                  \e[49m
-> \e[44m                                                                                  \e[49m
-> \e[44m                                                                                  \e[49m'
+echo  ' \e[44m                                                                                  \e[49m
+ \e[44m                                                                                  \e[49m
+ \e[44m                                                                                  \e[49m
+ \e[44m                              \e[47m\e[34m Debian \e[44m                                            \e[49m
+ \e[44m                                                                                  \e[49m
+ \e[44m           \e[39mAn exception 0E has occured at 0028:C7HE38B4 in DEBIAN GNU +           \e[49m
+ \e[44m           LINUX. This was called from 0xDED:DF7D4 in VFX:43J5G7H.                \e[49m
+ \e[44m           It may be possible to continue normally.                               \e[49m
+ \e[44m                                                                                  \e[49m
+ \e[44m           * Press any key to attempt to continue.                                \e[49m
+ \e[44m           * Press CTRL+ALT+DEL to restart your computer. You will                \e[49m
+ \e[44m           lose any unsaved information in all applications.                      \e[49m
+ \e[44m                                                                                  \e[49m
+ \e[44m                      Press any key to continue _                                 \e[49m
+ \e[44m                                                                                  \e[49m
+ \e[44m                                                                                  \e[49m
+ \e[44m                                                                                  \e[49m
+ \e[44m                                                                                  \e[49m'
